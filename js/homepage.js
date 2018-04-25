11c8b5bb2c48f9c4393677a34a788fee44357eb5
mohankarthik99 <32239797+mohankarthik99@users.noreply.github.com>
2018-04-26 00:18:17 +0530

    Changing

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,4 @@
-var cool = [];
-var raj = [];
+
 (function () {
 
 	var slideout = new Slideout({             ///slideout menu
@@ -46,79 +45,29 @@ var raj = [];
 	const logout = document.querySelector("#logout");
 	var docref = firestore.collection("teachers");
 	const checkdetails = document.getElementById("checkdetails");
-	var signout = false;
+	var signIn = false;
+	var signOut = false;
 
 
 
 	auth.onAuthStateChanged((authdata) => { //Main controller
 		if (authdata) {
-			nameDecide(auth);
+			signIn = true;
+			nameListener(auth);
+		} else if (signIn == false) {
+			window.location.href = "404.html"
 		}
-		else {
-			if (!(signout)) {
-				alert("Please Login first");
-				window.location.href = "index.html"
-			}
+		else if (signOut) {
+			window.location.href = "index.html"
 		}
 	});
 
 
-/*    Distinguishing between Signup and login and displaying the name of the user
-*/var nameDecide = function (auth) {
-		var final = "";
-		if (location.search) {
-			return function () {
-				var replacement = location.search.replace(/%20/g, "");
-				if (replacement.indexOf("%20") >= 0) {
-					replacement.replace(/%20/g, "");
-				}
-
-				var start = replacement.indexOf("=");
-				var checkPost = true;
-				var end = replacement.indexOf("&");
-				for (var i = start + 1; i < end; i++) {
-					final += replacement[i];
-				}
-				docref.where('UID', "==", auth.currentUser.uid).get().then(function (querySnapshot) {
-					querySnapshot.forEach((doc) => {
-						if (doc.data().UID == auth.currentUser.uid) {
-							checkPost = false;
-							final = doc.data().name;
-						}
-					});
-
-					if (checkPost) {
-						docref.add({
-							'UID': auth.currentUser.uid,
-							'name': final,
-							'email': auth.currentUser.email
-						}).catch((error) => {
-							alert("Something went wrong" + error.message);
-						});
-						return nameListener(auth); //Activate realtime listener for name and get the name
-					}// if new user
-
-					return nameListener(auth); //Activate realtime listener for name and get the name
-				}).catch(function (error) {
-					alert("error" + error.message);
-				});
-
-			}();
-
-		}
-		else {
-			return function () {
-				return nameListener(auth); //Activate realtime listener for name and get the name
-			}();
-		}
-	}
 
 
 	//Realtime listener for name
 	function nameListener(auth) {
 		docref.where("UID", "==", auth.currentUser.uid).onSnapshot((querySnapshot) => {
-			raj.push(querySnapshot);
-
 			querySnapshot.forEach((doc) => {
 				if (doc.exists) {
 					document.querySelector("#username").innerHTML = (doc.data().name);
@@ -209,21 +158,6 @@ var raj = [];
 		return uploads;
 	}
 
-	var youtubethumb = function (url) {
-		if (url.indexOf("youtube.com") >= 0) {
-			let imgid = url.substring(url.indexOf("=") + 1, url.length);
-			return "https://img.youtube.com/vi/" + imgid + "/0.jpg"
-		}
-		else if (url.indexOf("youtu.be.com") >= 0) {
-			var imgid = url.substring(url.lastIndexOf("/"), url.length);
-			return "https://img.youtube.com/vi/" + imgid + "/0.jpg"
-		}
-		else {
-			return "images/image-file.png";
-		}
-	}
-
-
 	async function represent(year, dpt) {
 		var $barelement;
 		var $template;
@@ -247,7 +181,7 @@ var raj = [];
 							});
 						});
 						for (let j = 0; j < dataArr.length; j++) {
-							$template = $("<div class='card'> <h3 style='color: #000' class='fontbold' id='titlecard'>" + dataArr[j][0] + "</h3> <div class='flexit'> <img class='cardimguser' src='" + youtubethumb(dataArr[j][2]) + "'> </div><h3 class='fontsemibold' style='color: #000;padding:5px 0px;'>Description</h3> <p class='text-center black fontsemibold' style='font-size: 1.4rem;padding: 10px' id='desccard'>" + dataArr[j][1] + "</p><a href=" + dataArr[j][2] + " target='_blank'><button class='btn btn-info extend fontsemibold'>View</button></a> <h4 class='fontsemibold black text-center'>Uploaded By" + dataArr[j][3] + "</h4></div>");
+							$template = $("<div class='card'> <h3 style='color: #000' class='fontbold' id='titlecard'>" + dataArr[j][0] + "</h3> <div class='flexit'> <img class='cardimguser' src='images/image-file.png'> </div><h3 class='fontsemibold' style='color: #000;padding:5px 0px;'>Description</h3> <p class='text-center black fontsemibold' style='font-size: 1.4rem;padding: 10px' id='desccard'>" + dataArr[j][1] + "</p><a href=" + dataArr[j][2] + " target='_blank'><button class='btn btn-info extend fontsemibold'>Download</button></a> <h4 class='fontsemibold black text-center'>Uploaded By" + dataArr[j][3] + "</h4></div>");
 							$(flexita[i]).append($template);
 						}
 
@@ -286,9 +220,7 @@ var raj = [];
 
 	logout.addEventListener("click", function () { //Logout button
 		firebase.auth().signOut().then(function () {
-			signout = true;
-			alert("You have been signed out");
-			window.open("index.html", "_self");
+			signOut = true;
 		}).catch(function (error) {
 			console.log(error.message);
 		});
